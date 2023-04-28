78b7daf636c3c33bc148005e9c98cab9048fa847
r_fujisaki <ryusukefujisaki@gmail.com>
2023-04-29 02:03:24 +0900

    Add C of CRUD

--- src/pages/crud/Crud.tsx
+++ src/pages/crud/Crud.tsx
@@ -4,22 +4,55 @@ import { formatDate } from '@/utilities/displayHelper'
 
 function Crud(): JSX.Element {
   const [cruds, setCruds]: Array<any> = useState([])
+  const [updated, setUpdated] = useState(0)
   useEffect(() => {
     axios.get('/cruds').then(response => {
       setCruds(cruds => cruds = response.data)
     })
-  }, [])
+  }, [updated])
+  const [createInput, setInputCreate]: Array<any> = useState()
+  function handleChangeCreate(event) {
+    setInputCreate((createInput: string | null) =>
+      createInput = event.target.value
+    )
+  }
+  function handleClickCreate(event) {
+    event.preventDefault()
+    axios.post('/cruds', { value: createInput }).then(() => {
+      setInputCreate((createInput: string | null) => {
+        createInput = null
+      })
+      document.getElementById('create-input').value = null
+      setUpdated((updated: number) => ++updated)
+    })
+  }
 
   return (
     <>
-      <p className="text-3xl text-left p-4">R</p>
-      <table className="table-fixed px-4 border-separate w-full bg-white dark:bg-slate-800">
+      <p className="text-3xl text-left ml-16 py-3">C</p>
+      <form className="flex">
+        <input
+          id="create-input"
+          className="flex ml-16 w-1/5 border rounded p-3 focus:outline-none"
+          type="text"
+          placeholder="value"
+          onChange={handleChangeCreate}
+        />
+        <input
+          className="bg-blue-500 hover:bg-blue-600 w-1/12 text-white ml-1 p-3 rounded focus:outline-none"
+          type="submit"
+          value="Submit"
+          onClick={handleClickCreate}
+        />
+      </form>
+      <p className="text-3xl text-left ml-16 py-3">R</p>
+      <table className="table-fixed px-16 border-separate w-full bg-white dark:bg-slate-800">
         <thead className="bg-slate-50 dark:bg-slate-700">
           <tr>
             {
               ['id', 'value', 'created_at', 'updated_at'].map((th, index) =>
                 <th
-                  className="w-1/2 border border-slate-300 dark:border-slate-600 font-semibold p-4 text-slate-900 dark:text-slate-200 text-left"
+                  className="w-1/2 border border-slate-300 dark:border-slate-600 font-semibold p-3  text-slate-500"
                   style={{textAlign: 'center'}}
                   key={index}
                 >
@@ -35,7 +68,7 @@ function Crud(): JSX.Element {
               {
                 [crud.id, crud.value, formatDate(crud.created_at), formatDate(crud.updated_at)].map((attribute, index) =>
                   <td
-                    className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400"
+                    className="border border-slate-300 dark:border-slate-700 p-3 text-slate-500"
                     key={`${crud.id}-${index}`}
                     >
                     {attribute}
